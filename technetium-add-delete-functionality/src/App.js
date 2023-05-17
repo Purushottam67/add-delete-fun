cc96754d5053d3f9d41c08c88c900c38e1b72f81
Purushottam mishra <78618623+Purushottam67@users.noreply.github.com>
2023-05-17 08:46:20 +0530

    Add files via upload

---
+++ technetium-add-delete-functionality/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import RemItem from './components/Atom/RemItem';
+
+function App() {
+  return (
+    <div className="App">
+      <RemItem/>
+    </div>
+  );
+}
+
+export default App;
